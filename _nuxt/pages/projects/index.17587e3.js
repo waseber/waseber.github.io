858fb6c5bc9905adca076fe0e9d24a9ac13c9e01
waseber <yvesgerber@gmail.com>
2020-07-17 00:12:51 -0400

    initial commit

---
+++ _nuxt/pages/projects/index.17587e3.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{194:function(e,n,t){"use strict";t.r(n);var r=t(9),component=Object(r.a)({},(function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("\n    Here are my projects!\n")])}),[],!1,null,null,null);n.default=component.exports}}]);
---
+++ _nuxt/static/1594955408/projects/genealogybank/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/projects/genealogybank", {data:[{}],fetch:[],mutations:void 0});
