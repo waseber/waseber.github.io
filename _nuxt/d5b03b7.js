1ea42192585198777f6bb22a54242eb2633527fa
waseber <yvesgerber@gmail.com>
2020-12-14 09:42:52 -0500

    adds transitions

---
+++ _nuxt/d5b03b7.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{216:function(e,n,t){"use strict";t.r(n);var r=t(10),component=Object(r.a)({},(function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("\n    Here are my projects!\n")])}),[],!1,null,null,null);n.default=component.exports}}]);
---
+++ _nuxt/static/1607956263/projects/reuters/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/projects/reuters", {data:[{}],fetch:[],mutations:[]});
